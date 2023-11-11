37cc2ff13bb0c1e4ff64128352d4e7ce2d6a7afb
usuario <usuario@UNI-ZITOR.mshome.net>
2023-11-11 15:49:04 +0100

    Merge branch 'master' of https://github.com/Davidsantamaria1111/brandubhhola.git

---
+++ brandhubzitor/src/brandubh/modelo/Tablero.java
@@ -0,0 +1,8 @@
+package brandubh.modelo;
+/**
+ *  Autor: Pablo Citores
+ *  Grupo 103
+ */
+public class Tablero {
+
+}
---
+++ brandhubzitor/src/brandubh/util/Coordenada.java
@@ -0,0 +1,22 @@
+package brandubh.util;
+/**
+ *  Autor: Pablo Citores
+ *  Grupo 103
+ *  Coordenadas de una celda con su valor de fila y de columna.
+ */
+
+public record Coordenada(int fila, int columna) { 
+	
+	public Coordenada clonar() {
+        return new Coordenada(this.fila, this.columna);
+    }
+	
+	public int getFila() {
+		return fila;
+	}
+	
+	public int getColumna() {
+		return columna;
+	}
+}
+
