fb010706d700dfc6dc11e4baab96c403101fc1af
usuario <usuario@UNI-ZITOR.mshome.net>
2023-11-17 17:54:29 +0100

    jugada.java sin fallos

--- src/brandubh/modelo/Jugada.java
+++ src/brandubh/modelo/Jugada.java
@@ -4,6 +4,7 @@ package brandubh.modelo;
  *  Autor: Pablo Citores y David Santamaría
  *  Grupo 103
  */
+
 import brandubh.util.Sentido;
 
 public class Jugada {
@@ -21,9 +22,9 @@ public class Jugada {
         int diffY = destino.getCoordenada().columna() - origen.getCoordenada().columna();
 
         if (diffX == 0 && diffY != 0) {
-            return (diffY > 0) ? Sentido.VERTICAL_S : Sentido.VERTICAL_N;
+            return (diffY > 0) ? Sentido.HORIZONTAL_E : Sentido.HORIZONTAL_O;
         } else if (diffY == 0 && diffX != 0) {
-            return (diffX > 0) ? Sentido.HORIZONTAL_E : Sentido.HORIZONTAL_O;
+            return (diffX > 0) ? Sentido.VERTICAL_S : Sentido.VERTICAL_N;
         }
 
         return null;
